f3070f0d561eea5c5272a5faec51f3ea2e25b3e5
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-20 11:34:54 +0100

    building search component for cities

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6ede6c029dmsh4ea88b8645a981fp1c5202jsn46b237e488ae',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
