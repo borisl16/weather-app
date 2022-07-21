58db1a7595502469b584a9d72da10518b0fa7245
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-21 22:09:02 +0100

    css done for static current weather component

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -14,7 +14,7 @@ const CurrentWeather = () => {
                 <p className="temperature">25°C</p>
                 <div className="details">
                     <div className="parameter-row">
-                        <span className="parameter-label">Details</span>
+                        <span className="parameter-label top">Details: </span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Feels like </span>
