41f9e4ffdc2d65fb62138da67c01d821fa50d16d
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-21 15:13:58 +0100

    start creating a current weather component

---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,14 @@
+import './current-weather.css';
+
+const CurrentWeather = () => {
+    return (
+        <div className="weather">
+            <div className="top">
+                <p className="city">Rome</p>
+                <p className="weather-description">Sunny</p>
+            </div>
+        </div>
+    )
+}
+
+export default CurrentWeather; 
