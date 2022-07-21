8119dbd5be560ed652c7887b5d2c85aee077b34e
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-21 15:42:21 +0100

    start with current weather css

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,8 +4,11 @@ const CurrentWeather = () => {
     return (
         <div className="weather">
             <div className="top">
-                <p className="city">Rome</p>
-                <p className="weather-description">Sunny</p>
+                <div>
+                    <p className="city">Rome</p>
+                    <p className="weather-description">Sunny</p>
+                </div>
+                <img alt="weather" className="weather-icon" src="icons/01d.png" />
             </div>
         </div>
     )
