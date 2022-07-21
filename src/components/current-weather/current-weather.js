073df7c6870dd46a1761389bcc941f7b59da5a41
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-21 21:54:49 +0100

    adding weather details

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -10,6 +10,30 @@ const CurrentWeather = () => {
                 </div>
                 <img alt="weather" className="weather-icon" src="icons/01d.png" />
             </div>
+            <div className="bottom">
+                <p className="temperature">25°C</p>
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label">Details</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like </span>
+                        <span className="parameter-value">28°C</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind </span>
+                        <span className="parameter-value">6 m/s</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity </span>
+                        <span className="parameter-value">13 %</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure </span>
+                        <span className="parameter-value">9 hPa</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
