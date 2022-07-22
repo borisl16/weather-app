d24b5559f30b55577a005e3af7e0811e4a3544b6
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-22 18:21:10 +0100

    fetching current weather api keys

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather.js';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
 
 function App() {
 
   const handleOnSearchChange = (searchData) => {
-      console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+    
+    const currentWeatheFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
   }
 
   return (
     <div className="container ">
-      <Search onSearchChange={handleOnSearchChange}/>
+      <Search onSearchChange={handleOnSearchChange} />
       <CurrentWeather />
     </div>
   );
-} 
+}
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '6ede6c029dmsh4ea88b8645a981fp1c5202jsn46b237e488ae',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 
+export const WEATHER_API_URL =  'https://api.openweathermap.org/data/2.5'; 
+export const WEATHER_API_KEY = '';
