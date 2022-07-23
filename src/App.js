e057d8f2965e791d65d627b33ed69938862370f4
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-23 09:47:25 +0100

    start working on forecast weather

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
 
 function App() {
 
+  const [currentWeather, setCurrentWeather] = useState(null); 
+  const [forecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
     
     const currentWeatheFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+    const forecastFetc = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+
+    Promise.all([currentWeatheFetch, forecastFetc])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+
+      setCurrentWeather({weatherResponse});
+      setForecast({forecastResponse}); 
+    })
   }
 
   return (
