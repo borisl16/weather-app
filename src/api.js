94f7a62be5a1f9388b20f8c40a2744c3e898e3ef
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-25 22:07:56 +0100

    no api commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '6ede6c029dmsh4ea88b8645a981fp1c5202jsn46b237e488ae',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 
 export const WEATHER_API_URL =  'https://api.openweathermap.org/data/2.5'; 
-export const WEATHER_API_KEY = 'a08c009397ce719319d7365143f8fc42';
+export const WEATHER_API_KEY = '';
