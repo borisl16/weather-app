0fc92719b33a5c01d6d45aefa93a6e1012bc8c9e
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-07-27 14:39:10 +0100

    added weekdays for the forecast

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CurrentWeather from './components/current-weather/current-weather.js';
 import Forecast from './components/forecast/forecast.js';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
 import { useState } from 'react';
-import Forecast from './components/forecast/forecast';
+
 
 function App() {
 
@@ -35,7 +35,7 @@ function App() {
     <div className="container ">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
-      <Forecast />
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,10 +1,33 @@
-import { Accordion } from "react-accessible-accordion";
+import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion";
 
-const Forecast = () => {
+
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const Forecast = ({ data }) => {
+
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+    console.log(forecastDays);
+
     return (
         <>
-            <label className="title">Daily</label>
-            <Accordion > </Accordion>
+            <label className="title">Daily:</label>
+            <Accordion allowZeroExpanded>
+                {data.list.splice(0, 7).map((item, index) => (
+                    <AccordionItem key={index}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="daily-item">
+                                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                                    <label className="day"></label>
+                                </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel> </AccordionItemPanel>
+                    </AccordionItem>
+                ))}
+
+            </Accordion>
         </>
     )
 }
